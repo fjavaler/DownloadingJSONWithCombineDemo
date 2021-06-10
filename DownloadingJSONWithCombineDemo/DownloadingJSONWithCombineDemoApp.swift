1c20f18d27db0c366b52ff9f3fe0d696eeb1fbc3
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-10 11:56:12 -0600

    Initial Commit

---
+++ DownloadingJSONWithCombineDemo/DownloadingJSONWithCombineDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadingJSONWithCombineDemoApp.swift
+//  DownloadingJSONWithCombineDemo
+//
+//  Created by Fred Javalera on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct DownloadingJSONWithCombineDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
